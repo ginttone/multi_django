aca5c6e1febbe71545bfee276155203b78d22eda
juni <alicelikesbab@gmail.com>
2021-07-09 18:18:51 +0900

    end

--- web_config/urls.py
+++ web_config/urls.py
@@ -19,8 +19,8 @@ from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
     path('home',views.index),
+    path('',views.index),
     path('index01',views.index01),
     path('index02',views.index02)
 ]
