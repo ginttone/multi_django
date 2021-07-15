13ef4723ec1a32a03653263fbdcd59c6c87f4131
juni <alicelikesbab@gmail.com>
2021-07-15 11:31:28 +0900

    modify

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=100)
-    public_date = models.DateTimeField()
-    votes = models.BigIntegerField()
+    question_text = models.CharField(max_length=100)#column(변수),datatype
+    public_date = models.CharField(max_length=100) #DateTimeField
+    votes = models.DecimalField(max_digits=20,decimal_places=10) #BigIntegerField
+
+class Economics(models.Model): # Model상속받아 기능을 향상시킴
+    title = models.CharField(max_length=100)#컬럼 이름title은 성격이 str이라
+    href = models.CharField(max_length=100) # href성격이 link 고 str이라
+    creat_date = models.CharField(max_length=100)#두 날짜 계산해야 할 때 DateTimeField사용
---
+++ scrapping/daum_economic_scrapping.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+res = requests.get('http://media.daum.net/economic/')
+
+import splite3
+
+
+if res.status_code == 200:
+    soup = BeautifulSoup(res.content, 'html.parser')
+    links = soup.find_all('a', class_='link_txt')
+    connect = sqlite3.connect('./db.sqlite3')
+    cursor = connect.cursor()
+
+    for link in links:
+        title = str.strip(link.get_text())
+        href = str.strip(link.get('href'))
+        print(title, ' : ', href)
+        try:
+            cursor.execute(
+                "insert into polls_economics(create_date, href, title) values(datetime('now'), ?, ?)",
+                           (href, title))
+        except:
+            pass
+        print(title, ' : ', href)
+    connect.commit()
